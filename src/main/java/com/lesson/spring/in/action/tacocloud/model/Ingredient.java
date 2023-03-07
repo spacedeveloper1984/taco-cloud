d42451c011504a039f809ad2111c59a3f50c26f0
ogayvoronskiy <ogayvoronskiy@rencredit.ru>
2023-03-07 21:26:56 +0300

    first and second part

---
+++ src/main/java/com/lesson/spring/in/action/tacocloud/model/Ingredient.java
@@ -0,0 +1,11 @@
+package com.lesson.spring.in.action.tacocloud.model;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+
+	private final String id;
+	private final String name;
+	private final IngredientType type;
+}
---
+++ src/main/java/com/lesson/spring/in/action/tacocloud/model/IngredientType.java
@@ -0,0 +1,5 @@
+package com.lesson.spring.in.action.tacocloud.model;
+
+public enum IngredientType {
+	WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+}
