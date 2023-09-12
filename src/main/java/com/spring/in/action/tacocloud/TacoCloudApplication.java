279d47cf9511fa9443c36e23987b8e4967245adb
spacedeveloper1984 <spacedeveloper1984@gmail.com>
2023-09-12 16:43:40 +0300

    chapter one

---
+++ src/main/java/com/spring/in/action/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.spring.in.action.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
